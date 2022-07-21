d18123f9359ba7674f14d94c3e8283953b77e891
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-07-21 22:28:30 +0200

    one project en cours

--- api/app/controller/projectController.js
+++ api/app/controller/projectController.js
@@ -10,6 +10,17 @@ const projectController ={
         } catch (error) {
             console.error(error);
         }
-    }
+    },
+    
+    async fetchOneProject(req, res){
+        const projectId = parseInt(req.params.id, 10)
+        try {
+            const oneProject = await projectDatamapper.oneProject(projectId)
+            return res.json(oneProject)
+        } catch (error) {
+            console.error(error);
+        }
+    },
+    
 }
 module.exports = projectController ;
