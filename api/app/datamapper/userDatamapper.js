3426a30e6bf222527136b9e1c5c0479b9a54b074
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-07-24 13:23:20 +0200

    first

--- api/app/datamapper/userDatamapper.js
+++ api/app/datamapper/userDatamapper.js
@@ -23,7 +23,7 @@ const userDatamapper = {
             const encryptedPassword = (await bcrypt.hash(req.password,10));
             
 
-            const sql = `INSERT INTO public.user( firstname, lastname, password, email, city, description, profil_picture, username_gith, url_github, url_gitlab, url_portfolio, url_linkedin, validationlink)
+            const sql = `INSERT INTO customer( firstname, lastname, password, email, city, description, profil_picture, username_gith, url_github, url_gitlab, url_portfolio, url_linkedin, validationlink)
             VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)`;
 
             const values = [req.firstname, req.lastname, encryptedPassword, req.email, req.city, req.description, req.profil_picture, req.username_gith, req.url_github, req.url_gitlab, req.url_portfolio,
@@ -35,7 +35,7 @@ const userDatamapper = {
         },
 
         async allUser (){
-        const sql = 'SELECT * FROM public.user'
+        const sql = 'SELECT * FROM customer'
         try {
             const result = await pool.query(sql);
             return result.rows;
@@ -45,7 +45,7 @@ const userDatamapper = {
         },
 
         async foundUserById (userId){
-        const sql = 'SELECT * FROM public.user WHERE id=$1'
+        const sql = 'SELECT * FROM customer WHERE id=$1'
         try {
             const result = await pool.query(sql, [userId]);
             return result.rows[0];
@@ -58,7 +58,7 @@ const userDatamapper = {
             console.log(email
             );
 
-        const sql = `SELECT * FROM public.user WHERE email = '${email}'`
+        const sql = `SELECT * FROM customer WHERE email = '${email}'`
         try {
             const result = await pool.query(sql)
             return result.rows[0]
@@ -69,7 +69,7 @@ const userDatamapper = {
         },
 
         async destroy (userId){
-        const sql = `DELETE FROM public.user WHERE id=$1`
+        const sql = `DELETE FROM customer WHERE id=$1`
         try {
             const result = await pool.query(sql, [userId]);
             return result.rows[0];
@@ -80,14 +80,12 @@ const userDatamapper = {
         },
 
         async update(body, userId) {
-            console.log(body);
+
             const fields = Object.keys(body).map((prop, index) => `"${prop}" = $${index + 1}`);
-            console.log(fields);
             const values = Object.values(body);
-            console.log(values);
             const savedPost = await pool.query(
                 `
-                    UPDATE public.user SET
+                    UPDATE customer SET
                         ${fields}
                     WHERE id = $${fields.length + 1}
                     RETURNING *
