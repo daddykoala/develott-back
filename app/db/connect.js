7acb1d232f75b49fc6b2d1ab58fc7048bcde6a09
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-07-26 19:10:33 +0200

    add pool ssl

--- app/db/connect.js
+++ app/db/connect.js
@@ -3,9 +3,9 @@ const Pool = require('pg-pool');
 
 const pool = new Pool({
     connectionString:process.env.DATABASE_URL,
-    ssl :{
-        rejectUnauthorized : false
-    }
+    ssl: {
+        rejectUnauthorized: false
+      }
 });
 
 pool.connect();
