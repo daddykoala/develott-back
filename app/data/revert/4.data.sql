7a52d2b9be53489768719dbcfa43364b874a266f
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-07-27 14:26:38 +0200

    test heroku

--- app/data/revert/4.data.sql
+++ app/data/revert/4.data.sql
@@ -2,6 +2,13 @@
 
 BEGIN;
 
-DELETE TABLE customer_has_project_role, project_has_job, project_has_techno, project, customer_has_techno, role, customer, job;
+-- DELETE TABLE customer_has_project_role;
+-- DELETE TABLE project_has_job;
+-- DELETE TABLE project_has_techno;
+-- DELETE TABLE project;
+-- DELETE TABLE customer_has_techno;
+-- DELETE TABLE role;
+-- DELETE TABLE customer;
+-- DELETE TABLE job;
 
 COMMIT;
