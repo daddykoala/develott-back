e6f57534a0c86948baa4fc896ff665bc60b69091
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-07-27 12:17:28 +0200

    push on heroku

--- app/data/deploy/1.init.sql
+++ app/data/deploy/1.init.sql
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS customer
     CONSTRAINT customer_pkey PRIMARY KEY (id),
     CONSTRAINT customer_job_id_fkey FOREIGN KEY (job_id)
     REFERENCES job (id) MATCH SIMPLE,
+    validation_link TEXT ,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW() ,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
--- migration/verify/04_data.sql
+++
@@ -1,7 +0,0 @@
--- Verify develott_sqitch:04_data on pg
-
-BEGIN;
-
--- XXX Add verifications here.
-
-ROLLBACK;
