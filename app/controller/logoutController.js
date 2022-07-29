b86755ae1ef07a2a7d225d89a93fb28c4b36b758
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-07-29 11:46:24 +0200

    update clearcookie

--- app/controller/logoutController.js
+++ app/controller/logoutController.js
@@ -3,20 +3,20 @@
 
 const handleLogout = (req, res) => {
 
-  const cookies = req.cookies
+  const cookies = req.cookies;
   if(!cookies?.jwt) {
     return res.sendStatus(204);
   }
   const refreshToken = cookies.jwt;
   
   // res.clearCookie("jwt", refreshToken, {httpOnly: true})
-  res.clearCookie("jwt", refreshToken, {httpOnly: true, sameSite: 'None', secure: true })
+  res.clearCookie("jwt", refreshToken, {httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
   //! En prod: si https ajouter secure: true !!!!
-  res.sendStatus(204)
+  res.sendStatus(204);
 
   
 }
 
 
 
-module.exports = {handleLogout}
+module.exports = {handleLogout};
