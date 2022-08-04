f74c60ee68c53fc64eabb0e81b9de15f18b8668c
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-08-04 16:53:29 +0200

    resolve error use

--- app/controller/logoutController.js
+++ app/controller/logoutController.js
@@ -14,7 +14,7 @@ const handleLogout = (req, res, next) => {
     const refreshToken = cookies.jwt;
     
     // res.clearCookie("jwt", refreshToken, {httpOnly: true})
-    res.clearCookie("jwt", {path: '/', httpOnly: true, sameSite: 'None', secure: true })
+    res.clearCookie("jwt", {path: '/', httpOnly: true, sameSite: 'None', secure: true });
     //! En prod: si https ajouter secure: true !!!!
     res.sendStatus(204);
   } catch (error) {
