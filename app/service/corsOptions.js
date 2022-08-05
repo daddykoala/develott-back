b6b91bf4c5c63d83ad20354bc3114198556d9e8a
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-08-05 14:02:45 +0200

    connexion heroku front

--- app/service/corsOptions.js
+++ app/service/corsOptions.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: 'http://localhost:3000',https://develott-front.herokuapp.com/',
+  origin: ['http://localhost:3000','https://develott-front.herokuapp.com'],
   optionsSuccessStatus: 200,
   credentials: true
 }
