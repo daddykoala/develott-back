47400b7e3e4708cd9ded91753d1772fbb4372354
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-08-07 19:03:45 +0200

    modif

--- app/controller/logoutController.js
+++ app/controller/logoutController.js
@@ -12,10 +12,12 @@ const handleLogout = (req, res) => {
       return res.sendStatus(204);
     }
     const refreshToken = cookies.jwt;
-    req.logOut();
+
     // res.clearCookie("jwt", refreshToken, {httpOnly: true})
     res.clearCookie("jwt", {path: '/', httpOnly: true, sameSite: 'None', secure: true });
-    res.clearCookie("connect.sid", {path: '/', httpOnly: true, sameSite: 'None', secure: true });
+    req.session.destroy((err) => {
+      res.clearCookie("connect.sid", {path: '/', httpOnly: true, secure: true, sameSite: 'None', proxy: true }).send('cleared cookie');
+   });
     //! En prod: si https ajouter secure: true !!!!
     res.sendStatus(204);
   } catch (error) {
