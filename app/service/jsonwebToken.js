5ef6dd9a1f5c9319271ea46c22e744d22c100258
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-08-07 10:29:04 +0200

    authenticateToken

--- app/service/jsonwebToken.js
+++ app/service/jsonwebToken.js
@@ -15,21 +15,21 @@ function generateRefreshToken(user) {
 }
 
 function authenticateToken(req, res, next) {
-    // //on recupere notre token dans le headers de la requete
-    // const authHeader = req.headers['authorization'];
-    // const token = authHeader && authHeader.split(' ')[1];
-    // //si le token n'existe pas on renvoi une erreur
-    // if (!token) {
-    //     return res.sendStatus(401);
-    // }
-    // //on verifie la veracité du token avec le secret.
-    // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    //     if (err) {
-    //         return res.sendStatus(403)//403 c'est forbidden;
-    //     }    
-    //     req.user = user;
+    //on recupere notre token dans le headers de la requete
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    //si le token n'existe pas on renvoi une erreur
+    if (!token) {
+        return res.sendStatus(401);
+    }
+    //on verifie la veracité du token avec le secret.
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.sendStatus(403)//403 c'est forbidden;
+        }    
+        req.user = user;
         next();
-    // });
+    });
 }
 
 module.exports = {
