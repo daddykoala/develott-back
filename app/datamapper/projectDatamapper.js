0f764c08d330286f2b8973194ede578d2dbc3426
daddykoala <moretticlement@outlook.fr>
2022-08-07 10:33:52 +0200

    route_projets_order by DESC

--- app/datamapper/projectDatamapper.js
+++ app/datamapper/projectDatamapper.js
@@ -69,7 +69,7 @@ const projectDatamapper = {
     },
 
     async allProjectLink (){
-        const sql = 'SELECT id, project, excerpt, picture, start_date, techno, role_id, firstname, lastname, c_profil_picture FROM public.v_project';
+        const sql = 'SELECT id, project, excerpt, picture, start_date, techno, role_id, firstname, lastname, c_profil_picture FROM public.v_project ORDER BY id DESC';
         const sql2='SELECT customer_id, role_id, project_id, role, firstname, lastname, profil_picture, job_id, job, techno_name FROM public.v_equipe';
         const sql3 ='SELECT * FROM public.v_project_has_job';
 
