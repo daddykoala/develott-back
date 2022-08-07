9b3a73777f26ee89440634d681839ac45862d2c4
Pierre-Antoine <pierreantoinepotage@gmail.com>
2022-08-07 15:07:04 +0200

    test passport

--- app/service/passport.js
+++ app/service/passport.js
@@ -9,8 +9,8 @@ passport.use(
 			clientSecret: "185247a333093dad2aeb7ff26b005cb4d61132ee",
 			callbackURL: "https://develott.herokuapp.com/v1/auth/github/callback",
 		},
+		console.log(profile),
 		async function (accessToken, refreshToken, profile, done) {
-			console.log(profile);
 			// const User = await foundByGithubUsername(profile._json.login);
 			// if (!User) {
 			// 	return done(null, false);
